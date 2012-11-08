7e65898e1a5eb2b7a4888f639e668f71c4c41fb0
Morgan <imdamorg@gmail.com>
2012-11-08 15:51:28 -0800

    Added boat hole

--- Rpg_project.rb
+++ Rpg_project.rb
@@ -7,25 +7,26 @@ class Rpggame < Chingu::Window
     Player.create.input = {holding_a: :move_left, holding_d: :move_right, holding_w: :move_up, holding_s: :move_down}
     Boat_door.create
     Goblin_enemy.create
-    $images =  ["Charecter back step 1 crop.png", "Charecter back step 2 crop.png", "Charecter left step 1 crop.png", "Charecter left step 2 crop.png", "Charecter right step 1 crop.png", "Charecter right step 2 crop.png", "Charecter step 1 crop.png", "Charecter step 2 crop.png" ]
+    $images = ["Charecter back step 1 crop.png", "Charecter back step 2 crop.png", "Charecter left step 1 crop.png", "Charecter left step 2 crop.png", "Charecter right step 1 crop.png", "Charecter right step 2 crop.png", "Charecter step 1 crop.png", "Charecter step 2 crop.png"]
     $images2 = ["Goblin step 1 crop.png", "Goblin step 2 crop.png", "Goblin  right step 1 crop.png", "Goblin right step 2 crop.png", "Goblin left step 1 crop.png", "Goblin left step 2 crop.png", "Goblin back step 1 crop.png", "Goblin back step 2 crop.png"]
     $moving_left = false
     $frame_rate = 0
-
+    $timer = 0
+    $frame_count = 0
 
 
   end
-
 
 
   def draw
     super
-    @background.draw(0,0,0,1.5,1.5)
-
+    @background.draw(0, 0, 0, 1.5, 1.5)
+    $frame_count += 1
   end
 end
 class Player < Chingu::GameObject
   attr_accessor :image
+
   def initialize
     super
     @image = Gosu::Image['Charecter crop.png']
@@ -39,9 +40,9 @@ class Player < Chingu::GameObject
   def move_left
     if $window.button_down?(Gosu::KbW) or $window.button_down?(Gosu::KbS)
       @x -= Math::sin(Math::PI/4) * 4
-      else
-        @x = @x - 4
-      end
+    else
+      @x = @x - 4
+    end
 
     if $frame_rate% 20 < 10
 
@@ -52,6 +53,7 @@ class Player < Chingu::GameObject
     $frame_rate += 1
 
   end
+
   def move_right
     if $window.button_down?(Gosu::KbW) or $window.button_down?(Gosu::KbS)
       @x += Math::sin(Math::PI/4) * 4
@@ -81,7 +83,6 @@ class Player < Chingu::GameObject
     $frame_rate += 1
   end
 
-
   def move_down
     @y = @y + 4
     if $frame_rate% 20 < 10
@@ -95,12 +96,13 @@ class Player < Chingu::GameObject
 end
 class Boat_door < Chingu::GameObject
   traits :collision_detection, :bounding_circle
+
   def initialize
     super(:image => Gosu::Image['Boat_Hole 2 crop.png'])
-    @factor_x = 1.3
-    @factor_y = 1.3
-    @x = 404.5
-    @y = 153.2
+    @factor_x = 1.0
+    @factor_y = 1.0
+    @x = 385
+    @y = 185
   end
 
 
@@ -110,79 +112,58 @@ class Goblin_enemy < Chingu::GameObject
 
   def initialize
     super(:image => Gosu::Image['Goblin crop.png'])
-    @factor_x = 0.5
-    @factor_y = 0.5
+    @factor_x = 0.3
+    @factor_y = 0.3
     @x = 450
     @y = 500
-    @up=0
-    @down=0
-    @left=0
-    @right=0
-
 
 
   end
+
   def update
-    @up = 0
-    if rand(0..20) == 1
-      while @up < 10
-        @y = @y - 4
-        @up = @up +1
+    super
+    @direction = rand(1..4) if $frame_count % 100 == 0
+    if @direction == 1
+      @x -= 3
+      @image = Gosu::Image["#{$images2[4]}"] if $frame_count % 16 < 8
+      @image = Gosu::Image["#{$images2[5]}"] if $frame_count % 16 >= 8
+      if @x <= 0
+        @direction = 2
       end
-      if $frame_rate% 20 < 10
-
-        @image = Gosu::Image["#{$images2[6]}"]
-      else
-        @image = Gosu::Image["#{$images2[7]}"]
+    elsif @direction == 2
+      @x += 3
+      @image = Gosu::Image["#{$images2[2]}"] if $frame_count % 16 < 8
+      @image = Gosu::Image["#{$images2[3]}"] if $frame_count % 16 >= 8
+      if @x >= $window.width
+        @direction = 1
       end
 
 
+    elsif @direction == 3
+      @y -= 3
+      @image = Gosu::Image["#{$images2[6]}"] if $frame_count % 16 < 8
+      @image = Gosu::Image["#{$images2[7]}"] if $frame_count % 16 >= 8
+      if @y <= 0
+        @direction = 4
+      end
+
+    elsif
+    @direction == 4
+      @y += 3
+      @image = Gosu::Image["#{$images2[0]}"] if $frame_count % 16 < 8
+      @image = Gosu::Image["#{$images2[1]}"] if $frame_count % 16 >= 8
+      if @y >= $window.height
+        @direction = 3
+      end
+
     end
+  end
 
-  @down = 0
-  if rand(0..20) == 2
-    while @down < 10
-      @y = @y + 4
-      @down = @down +1
-    end
+end
 
-  end
-  if $frame_rate % 20 < 10
 
-    @image = Gosu::Image["#{$images2[0]}"]
-  else
-    @image = Gosu::Image["#{$images2[1]}"]
-  end
-  @left = 0
-  if rand(0..20) == 3
-    while @left < 10
-      @x = @x - 4
-      @left = @left +1
-    end
 
-  end
-  if $frame_rate % 20 < 10
 
-    @image = Gosu::Image["#{$images2[2]}"]
-  else
-    @image = Gosu::Image["#{$images2[3]}"]
-  end
-  @right = 0
-  if rand(0..20) == 4
-    while @right < 10
-      @x = @x + 4
-      @right = @right +1
-    end
-
-  end
-  if $frame_rate % 20 < 10
-
-    @image = Gosu::Image["#{$images2[4]}"]
-  else
-    @image = Gosu::Image["#{$images2[5]}"]
-  end
-  end
-  end
 
 
 
